c09715db3f9af5b84cde4e8980e970c3ac118ddb
Le Thien <doiden1999@gmail.com>
2020-12-14 20:13:55 +0700

    Update View
    admin view
    user view

--- src/main/java/com/myclass/controller/HomeController.java
+++ src/main/java/com/myclass/controller/HomeController.java
@@ -6,16 +6,22 @@
 package com.myclass.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("home")
 public class HomeController {
+	@RequestMapping(value="/add", method = RequestMethod.GET)
+	public String add() {
+		System.out.println("Loi hdjahjk");
+		return "admin/user/add";	
+	}
 	@RequestMapping(value="", method = RequestMethod.GET)
 	public String index() {
-		System.out.println("Loi hdjahjk");
-		return "home/index";
+		System.out.println("Loi");
+		return "user/profile/index";
 		
 	}
 }
