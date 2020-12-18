156cda6432168d786dcc30921ece91fecd50dcdc
Le Thien <doiden1999@gmail.com>
2020-12-18 20:23:19 +0700

    Create CourseServiceImpl.java

---
+++ src/main/java/com/myclass/service/impl/CourseServiceImpl.java
@@ -0,0 +1,117 @@
+/**
+ * Dec 18, 2020
+ * 7:23:52 PM
+ * @author LeThien
+ */
+package com.myclass.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.myclass.dto.CourseDto;
+import com.myclass.entity.Course;
+import com.myclass.repository.CourseRepository;
+import com.myclass.service.CourseService;
+@Service
+public class CourseServiceImpl implements CourseService{
+
+	private CourseRepository courseRepository;
+	
+	/**
+	 * @param courseRepository
+	 */
+	public CourseServiceImpl(CourseRepository courseRepository) {
+		super();
+		this.courseRepository = courseRepository;
+	}
+
+	@Override
+	public List<CourseDto> getAll() {
+		List<Course> courses = courseRepository.findAll();
+		List<CourseDto> dtos = new ArrayList<CourseDto>();
+		for(Course course : courses) {
+			CourseDto dto = new CourseDto();
+			dto.setId(course.getId());
+			dto.setTitle(course.getTitle());
+			dto.setImage(course.getImage());
+			dto.setLeturesCount(course.getLeturesCount());
+			dto.setHourCount(course.getHourCount());
+			dto.setViewCount(course.getViewCount());
+			dto.setPrice(course.getPrice());
+			dto.setDiscount(course.getDiscount());
+			dto.setPromotionPrice(course.getPromotionPrice());
+			dto.setDescription(course.getDescription());
+			dto.setContent(course.getContent());
+			dto.setCategoryId(course.getCategoryId());
+			dto.setLastUpdate(course.getLastUpdate());
+			dtos.add(dto);
+		}
+		return dtos;
+	}
+
+	@Override
+	public CourseDto getById(int id) {
+		Course course = courseRepository.findById(id).get();
+		CourseDto dto = new CourseDto();
+		dto.setId(course.getId());
+		dto.setTitle(course.getTitle());
+		dto.setImage(course.getImage());
+		dto.setLeturesCount(course.getLeturesCount());
+		dto.setHourCount(course.getHourCount());
+		dto.setViewCount(course.getViewCount());
+		dto.setPrice(course.getPrice());
+		dto.setDiscount(course.getDiscount());
+		dto.setPromotionPrice(course.getPromotionPrice());
+		dto.setDescription(course.getDescription());
+		dto.setContent(course.getContent());
+		dto.setCategoryId(course.getCategoryId());
+		dto.setLastUpdate(course.getLastUpdate());
+		return dto;
+	}
+
+	@Override
+	public void save(CourseDto dto) {
+		Course course = new Course();
+		course.setTitle(dto.getTitle());
+		course.setImage(dto.getImage());
+		course.setLeturesCount(dto.getLeturesCount());
+		course.setHourCount(dto.getHourCount());
+		course.setViewCount(dto.getViewCount());
+		course.setPrice(dto.getPrice());
+		course.setDiscount(dto.getDiscount());
+		course.setPromotionPrice(dto.getPromotionPrice());
+		course.setDescription(dto.getDescription());
+		course.setContent(dto.getContent());
+		course.setCategoryId(dto.getCategoryId());
+		courseRepository.save(course);
+	}
+
+	@Override
+	public void edit(CourseDto dto) {
+		Course course = courseRepository.findById(dto.getId()).get();
+		if(course!=null) {
+			course.setTitle(dto.getTitle());
+			course.setImage(dto.getImage());
+			course.setLeturesCount(dto.getLeturesCount());
+			course.setHourCount(dto.getHourCount());
+			course.setViewCount(dto.getViewCount());
+			course.setPrice(dto.getPrice());
+			course.setDiscount(dto.getDiscount());
+			course.setPromotionPrice(dto.getPromotionPrice());
+			course.setDescription(dto.getDescription());
+			course.setContent(dto.getContent());
+			course.setCategoryId(dto.getCategoryId());
+			courseRepository.save(course);
+		}
+		
+	}
+
+	@Override
+	public void remove(int id) {
+		courseRepository.deleteById(id);
+		
+	}
+
+}
