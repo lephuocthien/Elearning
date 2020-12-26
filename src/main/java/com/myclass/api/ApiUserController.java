e146dba03cb119ed2e648f7a31e7784fab93781c
Le Thien <doiden1999@gmail.com>
2020-12-26 19:02:23 +0700

    Create ApiUplaodController.java

--- src/main/java/com/myclass/api/ApiUserController.java
+++ src/main/java/com/myclass/api/ApiUserController.java
@@ -43,8 +43,7 @@ import com.myclass.service.UserService;
 @RequestMapping("api/user")
 @CrossOrigin("*")
 public class ApiUserController {
-	@Autowired
-	private ServletContext servletContext;
+	
 	private UserService userService;
 	private CourseService courseService;
 
@@ -158,29 +157,5 @@ public class ApiUserController {
 		}
 	}
 	
-//	@PostMapping(value="upload", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-//	@ResponseBody
-//	public Object upload(@RequestParam() MultipartFile file) {
-//		String pathName = servletContext.getRealPath("/resources/upload/");
-//		File dir = new File(pathName);
-//		if(!dir.exists())
-//			dir.mkdirs();
-//		
-//		String pathSource = pathName +file.getOriginalFilename();
-//		File serverFile = new File(pathSource);
-//		FileOutputStream stream;
-//		try {
-//			stream = new FileOutputStream(serverFile);
-//			stream.write(file.getBytes());
-//			stream.close();
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		} catch(IOException e) {
-//			e.printStackTrace();
-//		}
-//		
-//		Map<String, String> result = new HashMap<String, String>();
-//		result.put("url", file.getOriginalFilename());
-//		return result;
-//	}
+	
 }
